66861db25084743d4b4ac140177d6609f633d467
Daniel Poulsen <danielkpoulsen@daniels-macbook-pro-2.local>
2017-02-01 21:31:15 +0000

    Initial Commit

---
+++ UPnP2Cast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UPnP2Cast
+//
+//  Created by Daniel K. Poulsen on 01/02/2017.
+//  Copyright © 2017 Daniel K. Poulsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
